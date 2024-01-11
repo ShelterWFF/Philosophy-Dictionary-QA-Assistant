733ecaa81fb02502d64354459bb0258c157d43bf
ShelterW <115854494+ShelterWFF@users.noreply.github.com>
2024-01-11 15:55:25 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,106 @@
+from langchain.vectorstores import Chroma
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+import os
+from LLM import InternLM_LLM
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+import gradio as gr
+
+def load_chain():
+    # 加载问答链
+    # 定义 Embeddings
+    embeddings = HuggingFaceEmbeddings(model_name="/root/data/model/bge-base-zh")
+
+    # 向量数据库持久化路径
+    persist_directory = '/root/data/class3/data_base/vector_db/chroma'
+
+    # 加载数据库
+    vectordb = Chroma(
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+        embedding_function=embeddings
+    )
+
+    # 加载自定义 LLM
+    llm = InternLM_LLM(model_path = "/root/data/model/Shanghai_AI_Laboratory/internlm-chat-7b")
+
+    # 定义一个 Prompt Template
+    template = """作为一名哲学问答助手，你的任务是根据所提供的上下文回答用户问题。要求如下：
+    1.如果上下文与用户问题无关，请回答“对不起，我不知道。”
+    2.回答请用中文，保持简洁专业。
+    上下文: 
+    ···
+    {context}
+    ···
+    用户问题: {question}
+    你的回答:"""
+
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],template=template)
+
+    # 运行 chain
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectordb.as_retriever(search_kwargs={"k": 2}),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt":QA_CHAIN_PROMPT}
+        )
+    
+    return qa_chain
+
+class Model_center():
+    """
+    存储检索问答链的对象 
+    """
+    def __init__(self):
+        # 构造函数，加载检索问答链
+        self.chain = load_chain()
+
+    def qa_chain_self_answer(self, question: str, chat_history: list = []):
+        """
+        调用问答链进行回答
+        """
+        if question == None or len(question) < 1:
+            return "", chat_history
+        try:
+            chat_history.append(
+                (question, self.chain({"query": question})["result"]))
+            # 将问答结果直接附加到问答历史中，Gradio 会将其展示出来
+            return "", chat_history
+        except Exception as e:
+            return e, chat_history
+
+
+# 实例化核心功能对象
+model_center = Model_center()
+# 创建一个 Web 界面
+block = gr.Blocks()
+with block as demo:
+    with gr.Row(equal_height=True):   
+        with gr.Column(scale=15):
+            # 展示的页面标题
+            gr.Markdown("""<h1><center>哲学问答小助手</center></h1>""")
+
+    with gr.Row():
+        with gr.Column(scale=4):
+            # 创建一个聊天机器人对象
+            chatbot = gr.Chatbot(height=450, show_copy_button=True)
+            # 创建一个文本框组件，用于输入 prompt。
+            msg = gr.Textbox(label="提问")
+
+            with gr.Row():
+                # 创建提交按钮。
+                db_wo_his_btn = gr.Button("Chat")
+            with gr.Row():
+                # 创建一个清除按钮，用于清除聊天机器人组件的内容。
+                clear = gr.ClearButton(
+                    components=[chatbot], value="Clear console")
+                
+        # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
+        db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[msg, chatbot], outputs=[msg, chatbot])
+
+    gr.Markdown("""提醒：<br>
+    1. 初始化数据库时间可能较长，请耐心等待。
+    2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+    """)
+gr.close_all()
+# 直接启动
+demo.launch()
