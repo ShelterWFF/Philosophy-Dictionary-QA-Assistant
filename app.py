b55341f4b4b94e9854dab62015781342f9bca0ac
ShelterW <115854494+ShelterWFF@users.noreply.github.com>
2024-01-11 17:27:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -80,38 +80,42 @@ class Model_center():
             return e, chat_history
 
 
-# 实例化核心功能对象
-model_center = Model_center()
-# 创建一个 Web 界面
-block = gr.Blocks()
-with block as demo:
-    with gr.Row(equal_height=True):   
-        with gr.Column(scale=15):
-            # 展示的页面标题
-            gr.Markdown("""<h1><center>哲学问答小助手</center></h1>""")
+if __name__ == "__main__":
 
-    with gr.Row():
-        with gr.Column(scale=4):
-            # 创建一个聊天机器人对象
-            chatbot = gr.Chatbot(height=450, show_copy_button=True)
-            # 创建一个文本框组件，用于输入 prompt。
-            msg = gr.Textbox(label="提问")
+    model_center = Model_center()
 
-            with gr.Row():
-                # 创建提交按钮。
-                db_wo_his_btn = gr.Button("Chat")
-            with gr.Row():
-                # 创建一个清除按钮，用于清除聊天机器人组件的内容。
-                clear = gr.ClearButton(
-                    components=[chatbot], value="Clear console")
-                
-        # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
-        db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[msg, chatbot], outputs=[msg, chatbot])
+    block = gr.Blocks()
+    with block as demo:
+        with gr.Row(equal_height=True):   
+            with gr.Column(scale=15):
+                gr.Markdown("""<h1><center>哲学问答小助手</center></h1>
+                            <center>基于InternLM-7b</center>
+                            """)
 
-    gr.Markdown("""提醒：<br>
-    1. 初始化数据库时间可能较长，请耐心等待。
-    2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
-    """)
-gr.close_all()
-# 直接启动
-demo.launch()
+        with gr.Row():
+            with gr.Column(scale=4):
+                chatbot = gr.Chatbot(height=450, show_copy_button=True)
+        
+                examples = [
+                    "马克思主义哲学有两个最显著的特点是什么？",
+                    "哲学的基本问题是什么？",
+                    "什么是辩证唯物主义？",
+                    "唯物主义哲学最根本的观点是什么？",
+                ]
+                msg = gr.Dropdown(choices=examples, label="问题（可选示例）", allow_custom_value=True)
+
+                with gr.Row():
+                    # 创建提交按钮。
+                    db_wo_his_btn = gr.Button("对话")
+                    clear = gr.ClearButton(components=[chatbot], value="清除")
+
+            # 设置按钮的点击事件。当点击时，调用上面定义的 qa_chain_self_answer 函数，并传入用户的消息和聊天历史记录，然后更新文本框和聊天机器人组件。
+            db_wo_his_btn.click(model_center.qa_chain_self_answer, inputs=[msg, chatbot], outputs=[msg, chatbot])
+            
+        gr.Markdown("""提醒：<br>
+        1. 初始化数据库时间可能较长，请耐心等待。
+        2. 使用中如果出现异常，将会在文本输入框进行展示，请不要惊慌。 <br>
+        """)
+    gr.close_all()
+    # 直接启动
+    demo.launch()
